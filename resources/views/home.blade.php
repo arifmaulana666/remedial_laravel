5a56e26981fa9b4445b297ec8e24e7af4e4fd98a
arifmaulana666 <bektidongo@gmail.com>
2020-08-10 00:14:33 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+    @if($errors)
+        @foreach ($errors->all() as $error)
+        <div class="alert alert-danger">
+            {{ $error }}
+        </div>
+        @endforeach
+    @endif
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Profile</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="{{ asset($user->photo) }}" alt="" srcset="" width="200"
+                                class="img-responsive mx-auto"
+                                style="display:block; margin-left: auto; margin-right: auto">
+                        </div>
+                        <div class="col-md-6">
+                            <table width="100%">
+                                <tr>
+                                    <td>Name</td>
+                                    <td>:</td>
+                                    <td>&nbsp;{{ $user->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Email</td>
+                                    <td>:</td>
+                                    <td>&nbsp;{{ $user->email }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <br>
+                    <button type="button" class="btn btn-primary btn-block btn-sm" data-toggle="modal"
+                        data-target="#exampleModal">
+                        Edit Profile
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <form action="{{ route('updateProfile', $user->id) }}" method="post" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <input type="hidden" name="user_id" value="{{ $user->id }}">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Name</label>
+                        <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}">
+                        @if ($errors->has('name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}">
+                        @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="">Photo</label>
+                        <input type="file" name="photo" id="photo" class="form-control" onchange="readURL(this);">
+                        <div style="margin-top: 10px">
+                        @if($user->photo != NULL)
+                            <img id="blah" src="{{asset($user->photo)}}" width="50%">
+                        @else
+                            <h1 style="color:red">Tidak ada Gambar</h1>
+                        @endif
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="">New Password</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                        @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="">Confirm New Password</label>
+                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<script>
+    setInterval(function () {
+        alert("Session Expired");
+        if (true) {
+            document.querySelector('#logout-form').submit();
+        }
+    }, 600000);
+</script>
+<script>
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
+@endsection
